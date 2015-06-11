c28f45ac6756f4bc977f0429498a12ae64ad0abc
Nathan Lodge <nathanlodge@icloud.com>
2015-06-11 15:05:08 -0400

    Configured js, css and index

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  page.init();
+})
+// variables
+var $list = $('.list-group');
+var $li = $('.message');
+var $messageForm = $('.messageForm')
+var $addInput = $('.messageInput')
+var listTemplateString = "<li class='list-group-item message'><span class='listItem'><%= listItem %></span></li>";
+
+
+var page = {
+  url: "http://tiy-fee-rest.herokuapp.com/collections/nl",
+  init: function () {
+    page.initStyling();
+    page.initEvents();
+  },
+  initStyling: function(){
+  //load to-do's
+  },
+  initEvents: function() {
+
+    $messageForm.submit(page.newmessage);
+  },
+  newmessage: function(event) {
+    event.preventDefault();
+    var compiledTemplate = _.template(listTemplateString);
+    var result = compiledTemplate({
+      listItem: $addInput.val()
+    });
+    $list.append(result);
+    $addInput.val("");
+  },
+
+  //ajax goes here
+}
