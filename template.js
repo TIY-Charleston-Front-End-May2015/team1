998f51e93db63dc6d130df2a52b4ac9a3444ad2a
Tina Conway <tinaconway@Tinas-MacBook-Air.local>
2015-06-12 13:15:46 -0400

    functionality working

--- template.js
+++ template.js
@@ -5,7 +5,7 @@ var templates = {};
 // single message template
 templates.message = [
   "<li class='list-group-item message'>",
-  "span class='username'><%= sender %></span>",
-  "<span class='listItem'><%= listItem %></span>",
+  "<span class='username'><%= sender %>: </span>",
+  "<span class='listItem'><%= msgBody %></span>",
   "</li>"
 ].join("");
