e5c1a39db0e948c116729f72b051756d8c3330a6
Nathan Lodge <nathanlodge@icloud.com>
2015-06-14 13:05:51 -0400

    All features implemented

--- template.js
+++ template.js
@@ -7,16 +7,16 @@ var templates = {};
 templates.addOtherMessage = [
   "<li class='list-group-item message'>",
   "<span class='username'>@<%= sender %>: </span>",
-  "<span id = <%= _id %> class='listItem <%= sender %>'><%= msgBody %></span>",
+  "<span id = <%= _id %> class='listItem'><%= msgBody %></span>",
   "</li>"
 ].join("");
 
 
 templates.addUserMessage = [
-  "<li class='list-group-item message'>",
-  "<span class='glyphicon glyphicon-remove-circle'></span>",
+  "<li class='list-group-item message userMessage'>",
+  "<span class='glyphicon glyphicon-remove-circle hide'></span>",
   "<span class='username'>@<%= sender %>: </span>",
-  "<span id = <%= _id %> class='listItem <%= sender %>'><%= msgBody %></span>",
+  "<span id = <%= _id %> class='listItem'><%= msgBody %></span>",
   "</li>"
 ].join("");
 
